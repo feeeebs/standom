dc645b5328e767a714314406e6d5786d21297e46
Phoebe Mueller <phoebe.mueller1@gmail.com>
2024-03-13 11:27:53 -0500

    maybe getting gh pages to work?? + filename cleanup

--- standom-frontend/vite.config.js
+++ standom-frontend/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(new URL('..', import.meta.url).pathname),
   resolve: {
   alias: {
 
