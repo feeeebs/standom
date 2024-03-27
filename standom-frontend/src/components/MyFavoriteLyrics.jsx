244ace1d7ab7028b9a44989edba0448156622b6b
Phoebe Mueller <phoebe.mueller1@gmail.com>
2024-03-27 09:21:26 -0500

    implementing snapshots method for favorites data queries

--- standom-frontend/src/components/MyFavoriteLyrics.jsx
+++ standom-frontend/src/components/MyFavoriteLyrics.jsx
@@ -1,18 +1,19 @@
 import { useCollection } from '@squidcloud/react';
 import React, { useState, useEffect } from 'react'
 import { Badge, Button, Card, ListGroup } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import { addNewFavoriteLyrics, updateLyricsFetched } from '../utilities/Redux/lyricsSlice';
+import { useSelector } from 'react-redux';
+//import { addNewFavoriteLyrics, updateLyricsFetched } from '../utilities/Redux/lyricsSlice';
 import { useNavigate } from 'react-router-dom';
 import FetchAlbumArtFromS3 from '../utilities/FetchAlbumArt';
+//import GetFavorites from '../utilities/GetFavorites';
 
 export default function MyFavoriteLyrics() {
 
     const [lyricInfo, setLyricInfo] = useState([]);
 
-    const dispatch = useDispatch();
+    //const dispatch = useDispatch();
     const userId = useSelector(state => state.user.userInfo.id);
-    const lyricsFetched = useSelector(state => state.lyrics.fetched);
+    //const lyricsFetched = useSelector(state => state.lyrics.fetched);
 
     const lyricsCollection = useCollection('song_lyrics', 'postgres_id');
     const favoritesCollection = useCollection('users_favorite_lyrics', 'postgres_id');
@@ -27,137 +28,130 @@ export default function MyFavoriteLyrics() {
       console.log('lyric info: ', lyricInfo);
 
     }, [lyricInfo])
-
-
-    // const favoritesSnapshot = favoritesCollection
-    //   .query()
-    //   .eq('user_id', userId)
-    //   .dereference()
-    //   .snapshots();
-    //             // console.log('favoritesSnapshot: ', favoritesSnapshot);
-    
-    // favoritesSnapshot.subscribe((users_favorite_lyrics) => {
-    //   console.log(
-    //     'Got new snapshot:',
-    //     users_favorite_lyrics.map((favorite) => favorite.data)
-    //   );
-    // });
-
-
-    // queries to fetch lyric, song, and album data for user's favorites
+  
     useEffect(() => {
-      if (!lyricsFetched) {
-          const getFavorites = async () => {
-            try {
-              // get list of favorites
-              const favoritesSnapshot = await favoritesCollection
-                .query()
-                .eq('user_id', userId)
-                .dereference()
-                .snapshot();
-                //console.log('favoritesSnapshot: ', favoritesSnapshot);
+      const subscription = favoritesCollection
+        .query()
+        .eq('user_id', userId)
+        .dereference()
+        .snapshots()
+        .subscribe({
+          next: (favoritesSnapshot) => {
+            console.log('Got new snapshot: ', favoritesSnapshot);
+            //console.log('lyric info: ', lyricInfo);
 
-                // array to stage lyric data before updating state
-                const newLyricBucket = [];
-
-                favoritesSnapshot.forEach(favoritesRow => {
-                  const { lyric_id, favorite_id } = favoritesRow;
-                  //console.log('favorites data: ', favoritesRow);
-                  //console.log('favorite_id: ', favorite_id);
-
-                  // look up lyric data based on user's specific list of favorites
-                  const getLyrics = async () => {
-                    // LYRIC
-                    const lyricSnapshot = await lyricsCollection
-                      .query()
-                      .eq('lyric_id', lyric_id)
-                      .dereference()
-                      .snapshot();
-                    const { lyric, song_id } = lyricSnapshot[0];
-                    //console.log('lyric: ', lyric);
-
-                    // LYRIC TAGS
-                    const userLyricTagsSnapshot = await userTagsCollection
-                      .query()
-                      .eq('favorite_id', favorite_id)
-                      .dereference()
-                      .snapshot();
-
-                    console.log('userLyricTagSnap: ', userLyricTagsSnapshot);
-
-                    const tagsBucket = [];
-                    if (userLyricTagsSnapshot) {
-                      userLyricTagsSnapshot.forEach(tag => {
-                        // once i figure out the join, change this to tag.tag to get the actual value
-                        // for now just displaying the ID to test it out
-                        tagsBucket.push(tag.tag_id);
-                      })
-                    }
-                    console.log('tagbucket: ', tagsBucket);
-
-                    // SONG
-                    const songSnapshot = await songsCollection
-                      .query()
-                      .eq('song_id', song_id)
-                      .dereference()
-                      .snapshot();
-                    const { song_title, album_id } = songSnapshot[0];
-                    // console.log('song: ', song_title);
-
-                    // ALBUM ART
-                    const albumSnapshot = await albumsCollection
-                      .query()
-                      .eq('album_id', album_id)
-                      .dereference()
-                      .snapshot();
-                    const { album_title, album_art_key } = albumSnapshot[0];
-                    //console.log('album: ', album_title);
-                    //console.log('album art key: ', album_art_key);
-
-                    // get album art from S3
-                    const albumArtUrl = await FetchAlbumArtFromS3(album_art_key);
-
-                    // PUT IT ALL TOGETHER
-                    // Create object to store song title, album title, lyric, and tags
-                    const lyricObject = {
-                      songTitle: song_title,
-                      albumTitle: album_title,
-                      lyric: lyric,
-                      albumArtUrl: albumArtUrl,
-                      tags: tagsBucket,
-                      favoriteId: favorite_id,
-                    }
-                    // add lyric to favorites array
-                    dispatch(addNewFavoriteLyrics([lyric]));
-                    newLyricBucket.push(lyricObject)
-                  }
-                  getLyrics();
-
-                })
-                // update state with new lyric entry
-                setLyricInfo(newLyricBucket);
-                dispatch(updateLyricsFetched());
-            } catch (error) {
-              console.error('Error fetching favorite lyrics: ', error);
-            }
+            const fetchFavorites = async () => {
+              try {
+                  // get list of favorites
+                  const favoritesSnapshot = await favoritesCollection
+                    .query()
+                    .eq('user_id', userId)
+                    .dereference()
+                    .snapshot();
+                    //console.log('favoritesSnapshot: ', favoritesSnapshot);
+        
+                    // array to stage lyric data before updating state
+                    const newLyricBucket = [];
+        
+                    favoritesSnapshot.forEach(favoritesRow => {
+                      const { lyric_id, favorite_id } = favoritesRow;
+                      //console.log('favorites data: ', favoritesRow);
+                      //console.log('favorite_id: ', favorite_id);
+        
+                      // look up lyric data based on user's specific list of favorites
+                      const getLyrics = async () => {
+                        // LYRIC
+                        const lyricSnapshot = await lyricsCollection
+                          .query()
+                          .eq('lyric_id', lyric_id)
+                          .dereference()
+                          .snapshot();
+                        const { lyric, song_id } = lyricSnapshot[0];
+                        //console.log('lyric: ', lyric);
+        
+                        // LYRIC TAGS
+                        const userLyricTagsSnapshot = await userTagsCollection
+                          .query()
+                          .eq('favorite_id', favorite_id)
+                          .dereference()
+                          .snapshot();
+        
+                        //console.log('userLyricTagSnap: ', userLyricTagsSnapshot);
+        
+                        const tagsBucket = [];
+                        if (userLyricTagsSnapshot) {
+                          userLyricTagsSnapshot.forEach(tag => {
+                            // once i figure out the join, change this to tag.tag to get the actual value
+                            // for now just displaying the ID to test it out
+                            tagsBucket.push(tag.tag_id);
+                          })
+                        }
+                        //console.log('tagbucket: ', tagsBucket);
+        
+                        // SONG
+                        const songSnapshot = await songsCollection
+                          .query()
+                          .eq('song_id', song_id)
+                          .dereference()
+                          .snapshot();
+                        const { song_title, album_id } = songSnapshot[0];
+                        // console.log('song: ', song_title);
+        
+                        // ALBUM ART
+                        const albumSnapshot = await albumsCollection
+                          .query()
+                          .eq('album_id', album_id)
+                          .dereference()
+                          .snapshot();
+                        const { album_title, album_art_key } = albumSnapshot[0];
+                        //console.log('album: ', album_title);
+                        //console.log('album art key: ', album_art_key);
+        
+                        // get album art from S3
+                        const albumArtUrl = await FetchAlbumArtFromS3(album_art_key);
+        
+                        // PUT IT ALL TOGETHER
+                        // Create object to store song title, album title, lyric, and tags
+                        const lyricObject = {
+                          songTitle: song_title,
+                          albumTitle: album_title,
+                          lyric: lyric,
+                          albumArtUrl: albumArtUrl,
+                          tags: tagsBucket,
+                          favoriteId: favorite_id,
+                        }
+                        // add lyric to favorites array
+                        newLyricBucket.push(lyricObject)
+                      }
+                      getLyrics();
+                      setLyricInfo(newLyricBucket);
+                    })
+                    // update state with new lyric entry
+                    lyricInfo.push(newLyricBucket);
+                } catch (error) {
+                  console.error('Error fetching favorite lyrics: ', error);
+                }
           };
-      
-      getFavorites();
+  
+          fetchFavorites();
 
-      }
-    }, [lyricsFetched, favoritesCollection, lyricsCollection, userId, lyricInfo]);
+          },
+          error: (error) => {
+            console.error('Error fetching favorites: ', error);
+          }
+        });
 
-    // WHY DOES REMOVING THIS UNUSED VARIABLE / USESELECTOR CALL BREAK THINGS???
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const favoriteLyrics = useSelector(state => state.lyrics.favoriteLyrics);
-
+      return () => {
+        subscription.unsubscribe();
+      };
+    }, [favoritesCollection, userId]);
+  
   const handleClick = () => {
       console.log('running handleLyricClick');
       navigate('/lyric-search');
     }
 
   const handleDelete = (favoriteId) => {
-
     console.log('favorite id being deleted: ', favoriteId);
       const deleteUserFavorite = async () => {
         // delete favorite from Db
@@ -175,7 +169,6 @@ export default function MyFavoriteLyrics() {
 }
 
 
-  // TO DO: ALLOW USER TO DELETE FAVORITES
   // TO DO: FILTER OUT FAVORITES THAT HAVE ALREADY BEEN ADDED OR MAKE SURE IT DOESN'T ADD DUPE ENTRIES
   // TO DO: INCLUDE "WHY YOU LOVE IT" TAGS - use badges to display?
   // TO DO: ADD FILTERS AND SORTING BY ALBUM/SONG
